2777b2329fe1eaff88affca3209f51fd673f3245
Ruslan Yupyn <r.yupyncrossplusplus.org>
2016-11-24 17:05:53 +0200

    added simple project;

---
+++ Fabric/Fabric/FabricLib.h
@@ -0,0 +1,13 @@
+//
+//  Fabric.h
+//  Fabric
+//
+//  Created by Ruslan Yupyn on 11/24/16.
+//  Copyright © 2016 CrossroadLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FabricLib : NSObject
+
+@end
---
+++ Fabric/Fabric/FabricLib.m
@@ -0,0 +1,13 @@
+//
+//  Fabric.m
+//  Fabric
+//
+//  Created by Ruslan Yupyn on 11/24/16.
+//  Copyright © 2016 CrossroadLabs. All rights reserved.
+//
+
+#import "FabricLib.h"
+
+@implementation FabricLib
+
+@end
